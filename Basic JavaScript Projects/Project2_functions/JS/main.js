da0572be9a546f6a0084efcf062185a5d1962252
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-16 20:11:30 -0700

    Update main.js

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function Second_Function() {
+    var A = "Boom text here!", B = "More text here!";
+    var result = A.fontcolor("blue");
+    document.getElementById("blue_text").innerHTML = result;
+}
