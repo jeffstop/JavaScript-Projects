ee950c687f3dd328cdb635bdcb93287e3a0a554b
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-16 18:39:52 -0700

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Should see this as an Alert!");
+
+document.write("You should see this somewhere");
+
+var A = "Basketball", B = "Football", C = "Other Football", D = "Hockey";
+document.write(D);
+
+var A = A.fontcolor("blue");
+document.write(A);
+
+var B = B.fontcolor("red");
+document.write(B);
+
+var C = C.fontcolor("purple");
+document.write(C);
+
+var D = D.fontcolor("orange");
+document.write(D);
