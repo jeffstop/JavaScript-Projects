d41b21c285173bc29ed09179940e46bcd3ffbc91
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-17 16:57:59 -0700

    Create main.js

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,20 @@
+var x = 6;
+var y = 2;
+var z = x - y;
+document.getElementById("Math").innerHTML = z;
+
+var t = x * y;
+document.getElementById("Math_2").innerHTML = t;
+
+var r = x / y;
+document.getElementById("Math_3").innerHTML = r;
+
+var w = (x + y) * x / y;
+document.getElementById("Math_4").innerHTML = w;
+
+var a = 5;
+var b = 2;
+var c = a % b;
+document.getElementById("Math_5").innerHTML = c;
+
+document.getElementById("Math_6").innerHTML = -x;
