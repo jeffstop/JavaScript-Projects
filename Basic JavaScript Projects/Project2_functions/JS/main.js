3c9ea7b6120c1d25097d6a3d301ed63be572de8f
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-17 09:43:59 -0700

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,11 @@
-function Second_Function() {
-    var A = "Boom text here!", B = "More text here!";
-    var result = A.fontcolor("blue");
-    document.getElementById("blue_text").innerHTML = result;
+function Second_Function() {        // Defining a function
+    var A = "Boom text here!";      // Defining A variable with a string value
+    var result = A.fontcolor("blue");       // Using fontcolor to make the color blue
+    document.getElementById("blue_text").innerHTML = result;        // Defining which element will use the function
+}
+
+function myFunction() {         // Defining a function
+    var sentence = "I am learning";     // Defing sentence variable with a string value
+    sentence += " a lot from this course!";         // Using an operator to concatenate a string
+    document.getElementById("Concatenate").innerHTML = sentence;        // Defining which element will use the function
 }
