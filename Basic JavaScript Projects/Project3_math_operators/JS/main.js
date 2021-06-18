e5a0bd24e3ebb4f554ba9d3e7aca568eb384fe2e
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-17 19:11:36 -0700

    Updated functions

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,44 +1,49 @@
-var x = 6;      // Defining x variable as a value of 6
-var y = 2;      // Defining y variable as a value of 2
-var z = x - y;      // Defining z variable as a value of y variable subtracted from x variable
-document.getElementById("Math").innerHTML = z;      // Displaying result of z variable
-
-var q = x + 3;      // Defining q variable as the result of the x variable added to 3
-document.getElementById("Math_1").innerHTML = q;        // Displaying result of q variable
-
-var t = x * y;      // Defining t variable as the product of x variable and y variable
-document.getElementById("Math_2").innerHTML = t;         // Displaying result of t variable
-
-var r = x / y;      // Defining r variable as the result of x divided by y
-document.getElementById("Math_3").innerHTML = r;         // Displaying result of r variable
-
-var w = (x + y) * x / y;        // Defining w variable as the result of this equation
-document.getElementById("Math_4").innerHTML = w;         // Displaying result of w variable
-
-var a = 5;      // Defining a variable as a value of 5
-var b = 2;      // Defining b variable as a value of 2
-var c = a % b;      // Defining c variable as the modulus remainder of a divided by b
-document.getElementById("Math_5").innerHTML = c;         // Displaying result of c variable
-
-document.getElementById("Math_6").innerHTML = -x;        // Displaying result of negation of x variable
-
-var j = 7;      // Defining j variable as a value of 7
-j++;            // Incrementing j variable by a value of 1
-document.write(j);      // Displaying result of incrementing j variable
-
-var k = 12      // Defining k variable as a value of 12
-k--;            // Decrementing j variable by a value of 1
-document.write(k);      // Displaying result of decrementing k variable (This will appear next to the result of incrementing j variable. The last line will display 811, but is really 8 & 11. I'm choosing not to create a space here as this is only a demonstration.)
-
-window.alert(Math.random());        // Displaying random number between 1 and 0 as an alert
-
-window.alert(Math.random() * 100);      // Displaying random number between 0 and 100 as an alert
-
-document.getElementById("Math_7").innerHTML = Math.round(Math.PI);      // Displaying Pi to the nearest round number. This will display on top of the increment and decrement functions, because this function displays in a specified <p> with id "Math_7"
-
-
-// This function is not displaying and I don't understand why. If I move document.getElementById outside of the curly brackets it still won't work (says the variable is declared but the value is never read)
+// Addition function
 function addition_Function() {
     var addition = 2 + 2
-    document.getElementById("Math_8").innerHTML = "2 + 2 = " + addition;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+
+// Subtraction function
+function subtraction_Function() {
+    var subtraction = 4 - 2
+    document.getElementById("Math_1").innerHTML = "4 - 2 = " + subtraction;
+}
+
+// Multiplication function
+function multiplication_Function() {
+    var multiplication = 4 * 2
+    document.getElementById("Math_2").innerHTML = "4 * 2 = " + multiplication;
+}
+
+// Division function
+function division_Function() {
+    var division = 8 / 2
+    document.getElementById("Math_3").innerHTML = "8 / 2 = " + division;
+}
+
+// Modulus operator
+function modulus_Operator() {
+    var Simple_Math = 27 % 4;
+    document.getElementById("Math_4").innerHTML = "When you divide 27 by 4 you have a remainder of: " + Simple_Math;
+}
+
+// Random number function
+function random_number() {
+    var random = Math.random();
+    document.getElementById("Math_5").innerHTML = "Random number is: " + random;
+}
+
+// Increment function
+function increment_Function() {
+    var x = 5;
+    x++;
+    document.getElementById("Math_6").innerHTML = "5 incremented once is: " + x;
+}
+
+// Decrement function
+function decrement_Function() {
+    var y = 5;
+    y--;
+    document.getElementById("Math_7").innerHTML = "5 decremented once is: " + y;
 }
