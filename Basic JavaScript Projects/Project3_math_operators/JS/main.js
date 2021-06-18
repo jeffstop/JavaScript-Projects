cb0b4310348b751bb178580823b1f14b7079c90f
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-17 17:17:11 -0700

    Update main.js

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,20 +1,32 @@
-var x = 6;
-var y = 2;
-var z = x - y;
-document.getElementById("Math").innerHTML = z;
+var x = 6;      // Defining x variable as a value of 6
+var y = 2;      // Defining y variable as a value of 2
+var z = x - y;      // Defining z variable as a value of y variable subtracted from x variable
+document.getElementById("Math").innerHTML = z;      // Displaying result of z variable
 
-var t = x * y;
-document.getElementById("Math_2").innerHTML = t;
+var t = x * y;      // Defining t variable as the product of x variable and y variable
+document.getElementById("Math_2").innerHTML = t;         // Displaying result of t variable
 
-var r = x / y;
-document.getElementById("Math_3").innerHTML = r;
+var r = x / y;      // Defining r variable as the result of x divided by y
+document.getElementById("Math_3").innerHTML = r;         // Displaying result of r variable
 
-var w = (x + y) * x / y;
-document.getElementById("Math_4").innerHTML = w;
+var w = (x + y) * x / y;        // Defining w variable as the result of this equation
+document.getElementById("Math_4").innerHTML = w;         // Displaying result of w variable
 
-var a = 5;
-var b = 2;
-var c = a % b;
-document.getElementById("Math_5").innerHTML = c;
+var a = 5;      // Defining a variable as a value of 5
+var b = 2;      // Defining b variable as a value of 2
+var c = a % b;      // Defining c variable as the modulus remainder of a divided by b
+document.getElementById("Math_5").innerHTML = c;         // Displaying result of c variable
 
-document.getElementById("Math_6").innerHTML = -x;
+document.getElementById("Math_6").innerHTML = -x;        // Displaying result of negation of x variable
+
+var j = 7;      // Defining j variable as a value of 7
+j++;            // Incrementing j variable by a value of 1
+document.write(j);      // Displaying result of incrementing j variable
+
+var k = 12      // Defining k variable as a value of 12
+k--;            // Decrementing j variable by a value of 1
+document.write(k);      // Displaying result of decrementing k variable (This will appear next to the result of incrementing j variable. The last line will display 811, but is really 8 & 11. I'm choosing not to create a space here as this is only a demonstration.)
+
+window.alert(Math.random());        // Displaying random number between 1 and 0 as an alert
+
+window.alert(Math.random() * 100);      // Displaying random number between 0 and 100 as an alert
