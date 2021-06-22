184e2b532adc9393c9c7a0ca63e5b0105865825d
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-22 10:25:25 -0700

    Update main.js

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -3,3 +3,22 @@ function Vote_Function() {
     var Vote = (Age < 18) ? "Too young":"Old enough";
     document.getElementById("vote").innerHTML = Vote + " to vote.";
 }
+
+class Vehicle {
+    constructor(Make, Model, Year, Color) {
+        this.Vehicle_Make = Make;
+        this.Vehicle_Model = Model;
+        this.Vehicle_Year = Year;
+        this.Vehicle_Color = Color;
+    }
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
+}
