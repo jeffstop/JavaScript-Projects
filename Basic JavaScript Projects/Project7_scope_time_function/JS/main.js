7a7b8c028b6959bcbcc57fc1b457e4cb6ab673d5
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-22 15:34:25 -0700

    Update main.js

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -19,3 +19,15 @@ function Add_Numbers_3() {
     console.log(12 + y + "<br>");
 }
 Add_Numbers_3();
+
+function conditional_Function() {
+    if (new Date().getHours() <20) {
+        document.getElementById("if_statement").innerHTML = "How is your day going?";
+    }
+}
+
+function another_Function() {
+    if (7 > 2)  {
+        document.getElementById("another_one").innerHTML = "7 is greater than 2";
+    }
+}
