5d73470f4fb1b0de7fe6b03b2d726652a1e626f0
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-22 13:44:36 -0700

    Update main.js

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,21 @@
+var X = 15;
+function Add_Numbers_1() {
+    document.write(13 + X + "<br>");
+}
+Add_Numbers_1();
+
+function Add_Numbers_2() {
+    var y = 10;
+    document.write(18 + y + "<br>");
+}
+Add_Numbers_2();
+
+function Add_Numbers_3() {
+    document.write(12 + y + "<br>");
+}
+Add_Numbers_3();
+
+function Add_Numbers_3() {
+    console.log(12 + y + "<br>");
+}
+Add_Numbers_3();
