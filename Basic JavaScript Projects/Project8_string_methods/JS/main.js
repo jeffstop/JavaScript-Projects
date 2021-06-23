09b6bafbe721f4a96d082d058d71838b2f97f170
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-23 15:09:50 -0700

    Update main.js

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -6,3 +6,21 @@ function Sentence() {
     var whole_sentence = A.concat(B, C, D);
     document.getElementById("concatenate").innerHTML = whole_sentence;
 }
+
+function slice_Method() {
+    var Sentence = "Here is a sentence to take a slice out of.";
+    var Section = Sentence.slice(27,34);
+    document.getElementById("slice").innerHTML = Section;
+}
+
+function uppercase_Function() {
+    var str = "This should all be in uppercase.";
+    var res = str.toUpperCase();
+    document.getElementById("uppercase").innerHTML = res;
+}
+
+function search_Function() {
+    var phrase = "The search method will find the position of 'method'";
+    var n = phrase.search("method");
+    document.getElementById("search").innerHTML = n;
+}
