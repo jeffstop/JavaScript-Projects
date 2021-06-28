a8605e5ca06b8b7a0fa730d37443bf97dfa96471
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-06-28 15:35:48 -0700

    Update main.js

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,4 @@
+// Call loop while X < 13
 function Call_Loop() {
     var Digit = "";
     var X = 2;
@@ -6,4 +7,15 @@ function Call_Loop() {
         X++;
     }
     document.getElementById("Loop").innerHTML = Digit;
+}
+
+// For loop, list of instruments
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
 }
